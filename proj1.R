afc6dab2c47e83b02cf958ff734678b2fb8feb87
Daiki Maruyama <D.Maruyama@sms.ed.ac.uk>
2023-09-26 14:53:48 +0100

    adding the codes of step3

--- proj1.R
+++ proj1.R
@@ -1,1 +1,9 @@
-#group project 1
+## Frans : 
+## Nathan :
+## Daiki Maruyama: s2547603
+
+## Contribution to this project
+
+setwd("C:/Users/maruw/ASEAN-Group-25") ## comment out of submitted
+a <- scan("4300-0.txt",what="character",skip=73,nlines=32858-73)
+a <- gsub("_(","",a,fixed=TRUE)
