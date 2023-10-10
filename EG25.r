6ed100cb5ff640401c0f2ab3ea63b12d9bc54cc0
Daiki Maruyama <D.Maruyama@sms.ed.ac.uk>
2023-10-10 14:28:37 +0100

    create a format

--- EG25.r
+++ EG25.r
@@ -1,1 +1,23 @@
-#Group 25
+## Group 25
+## Frans : s2591760
+## Daiki : s2547603
+## Nathan : s2524152
+
+
+## Contribution to this project
+## All group members Collaborated together to create the simulation of the 50-word sections from the model
+## All group members reviewed the entire code and wrote the comments together 
+## Frans (35%): Modified the code that replaces words that most often start with a capital letter in the main text
+## Daiki (32%): Handled the data pre-processing part of the task
+## Nathan (33%): Responsible for creating the matrices of common word triplets and pairs
+
+
+#######################################################################################################################
+
+
+
+
+
+#######################################################################################################################
+
+
