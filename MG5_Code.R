68514725f27d6117ee9ce4ee19989814e1f99fe5
Daiki Maruyama <D.Maruyama@sms.ed.ac.uk>
2023-10-14 18:47:46 +0100

    change some codes in MG5 file

--- MG5_Code.R
+++ MG5_Code.R
@@ -1,44 +1,46 @@
-# Function to simulate M/G/1 queue for a fixed duration
+# Function to simulate M/G/5 queue for a fixed duration
 
-simulate_MG1_fixed_duration <- function(arrival_rate, service_min, service_max) {
-        simulation_duration <- 7200  # Duration in seconds of queue
-        arrival_time <- 0  # Simulation clock
+simulate_MG5 <- function(mf, a.rate, trf, tmf) {
+        sim_duration <- 7200  # Duration in seconds of queue
+        checkin_time <- 5400  #
+        station <- matrix(0, 1, 5)
+        counter <- 0 # Count until time 7200
+        a.time <- 0  # Arrival time
         starting_service <- numeric(0)  # Time in which the customer in queue starts to be served
         finishing_time <- numeric(0)  # Time in which the customer is finished being served
-        
-        while (arrival_time < simulation_duration) {
-                TBA <- rexp(1, rate = arrival_rate) # Time before arrival 
-                arrival_time <- arrival_time + TBA
-                
-                if (arrival_time < simulation_duration) {
-                        # Simulate service time only if the arrival time is within the simulation duration
-                        service_time <- runif(1, min = service_min, max = service_max)
-                        
-                        # time in which the customer is finished being served 
-                        # is the sum of starting service time and actual service time
-                        finishing_time <- c(finishing_time, arrival_time + service_time)
-                        starting_service <- c(starting_service, arrival_time)
-                }
+        while (counter < sim_duration) {
+            while (a.time < checkin_time) {
+                    TBA <- rexp(1, rate = a.rate) # Time before arrival 
+                    a.time <- a.time + TBA
+                    # Simulate service time
+                    service_time <- runif(1, min = tmf, max = tmf + trf)
+                    
+                    # time in which the customer is finished being served 
+                    # is the sum of starting service time and actual service time
+                    finishing_time <- c(finishing_time, a.time + service_time)
+                    starting_service <- c(starting_service, a.time)
+                    
+            }
+            
+            # Calculate the time each customer spends waiting in line
+            Wq <- finishing_time - starting_service
+            
+            # Return results
+            return(list(starting_service = starting_service, 
+                        finishing_time = finishing_time, 
+                        Wq = Wq))
         }
-        
-        # Calculate the time each customer spends waiting in line
-        Wq <- finishing_time - starting_service
-        
-        # Return results
-        return(list(starting_service = starting_service, 
-                    finishing_time = finishing_time, 
-                    Wq = Wq))
-}
+    }
 
 # Example simulation for 7200 seconds
 set.seed(123)
-simulation_results <- simulate_MG1_fixed_duration(arrival_rate = 0.1, service_min = 30, service_max = 70)
+sim_results <- simulate_MG5(a.rate = 0.1, service_min = 30, service_max = 70)
 
 # Display results
-cat("Arrival Times:", simulation_results$starting_service, "\n")
-cat("Departure Times:", simulation_results$finishing_time, "\n")
-cat("Response Times:", simulation_results$Wq, "\n")
+cat("Arrival Times:", sim_results$starting_service, "\n")
+cat("Departure Times:", sim_results$finishing_time, "\n")
+cat("Response Times:", sim_results$Wq, "\n")
 
 # Display average waiting time
-average_waiting_time <- mean(simulation_results$Wq, na.rm = TRUE)
+average_waiting_time <- mean(sim_results$Wq, na.rm = TRUE)
 cat("Average Waiting Time:", average_waiting_time, "\n")
