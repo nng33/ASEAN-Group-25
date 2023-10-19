de9dceac9337fef1a30789b011aeac0bfd27efdb
Adlensius Fransiskus Djunaedi <s2591760@ed.ac.uk>
2023-10-19 15:52:25 +0100

    plot

--- frans18.R
+++ frans18.R
@@ -439,7 +439,7 @@ qsim <- function(mf, mb, a.rate, trb, trf, tmb, tmf, maxb){
               fa = fa, fq = fq, ft = ft, fb = fb, fe = fe, fs = fs))
 }
 
-set.seed(1)
+set.seed(100)
 
 sim <- qsim(mf=5,mb=5,a.rate=.1,trb=40,trf=40,tmb=30,tmf=30,maxb=20)
 
@@ -450,27 +450,40 @@ sim_40
 
 simt <- qsim(mf=5,mb=5,a.rate=0.9,trb=40,trf=5,tmb=100,tmf=5,maxb=2)
 
-tail(sim_40$nf, n = 1000)
-
 # Plot
 # Set up a 2x2 grid for plots
 par(mfrow = c(2, 2))  
 
 # Plot 1: French and British queue lengths over time
-plot(1:7200, sim$nf, type = "l", xlab = "Time (seconds)", ylab = "French Queue Length")
+plot(1:7200, sim$nf, type = "l", xlab = "Time (seconds)", ylab = "Queue Length", ylim = c(0,20))
 lines(1:7200, sim$nb, col = "red")
-# legend("topright", legend = c("French Queue", "British Queue"), col = c("black", "red"), lty = 1)
+title("Average Queue Length When tmb = 30")
+legend("topleft", legend = c("French Queue", "British Queue"), col = c("black", "red"), lty = 1, bty = "n")
 
 # Plot 2: Expected queuing time over time
-plot(1:7200, sim$eq, type = "l", xlab = "Time (seconds)", ylab = "Expected Queuing Time")
+plot(1:7200, sim$eq, type = "l", xlab = "Time (seconds)", ylab = "Expected Queuing Time", ylim = c(0,1000))
+title("Expected Queueing Time When tmb = 30")
 
 # Plot 3: French and British queue lengths over time
-plot(1:7200, sim_40$nf, type = "l", xlab = "Time (seconds)", ylab = "French Queue Length")
+plot(1:7200, sim_40$nf, type = "l", xlab = "Time (seconds)", ylab = "Queue Length", ylim = c(0,20))
 lines(1:7200, sim_40$nb, col = "red")
-# legend("topright", legend = c("French Queue", "British Queue"), col = c("black", "red"), lty = 1)
+title("Average Queue Length When tmb = 40")
+legend("topleft", legend = c("French Queue", "British Queue"), col = c("black", "red"), lty = 1, bty = "n")
 
 # Plot 4: Expected queuing time over time
-plot(1:7200, sim_40$eq, type = "l", xlab = "Time (seconds)", ylab = "Expected Queuing Time")
+plot(1:7200, sim_40$eq, type = "l", xlab = "Time (seconds)", ylab = "Expected Queuing Time", ylim = c(0,1000))
+title("Expected Queueing Time When tmb = 40")
+
+
+# Estimation
+# Calculate the probability that at least one car miss the ferry departure
+missing <- 0
+for (i in 1:100) {
+  qsim(mf=5,mb=5,a.rate=.1,trb=40,trf=40,tmb=40,tmf=30,maxb=20)
+  if (sum(busy) > 0) {
+    missing <- missing + 1
+  }
+}
 
 
 
