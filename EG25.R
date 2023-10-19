9bb382fa449834db8966c38907f17aa18fd89107
Daiki Maruyama <D.Maruyama@sms.ed.ac.uk>
2023-10-19 03:09:39 +0100

    created format

--- EG25.R
+++ EG25.R
@@ -5,27 +5,60 @@
 
 
 ## Contribution to this project
-## All group members Collaborated together to create the simulation of the 50-word sections from the model
-## All group members reviewed the entire code and wrote the comments together 
-## Frans (35%): Modified the code that replaces words that most often start with a capital letter in the main text
-## Daiki (32%): Handled the data pre-processing part of the task
-## Nathan (33%): Responsible for creating the matrices of common word triplets and pairs
+## Frans (%):  Daiki (%):  Nathan (%): 
 
 
 #######################################################################################################################
 
-
-
-
-
-#######################################################################################################################
 
 # setwd("C:/Users/maruw/ASEAN-Group-25") ## set the working directory
 
-qsim <- function(mf, mb, a.rate, trb, trf, tmb, tmf, maxb){
+# qsim function
+qsim <- function(mf, mb, a.rate, trb, trf, tmb, tmf, maxb) {
   
+  set.seed(11)
   
   
 }
 
-qsim(5,5,.1,40,40,30,30,20)
+sim <- qsim(mf=5,mb=5,a.rate=.1,trb=40,trf=40,tmb=30,tmf=30,maxb=20); sim
+sim_40 <- qsim(mf=5,mb=5,a.rate=.1,trb=40,trf=40,tmb=40,tmf=30,maxb=20); sim_40
+
+
+# Plot
+# Set up a 2x2 grid for plots
+par(mfrow = c(2, 2))  
+
+# Plot 1: French and British queue lengths over time
+plot(1:7200, sim$nf, type = "l", xlab = "Time (seconds)", ylab = "French Queue Length")
+lines(1:7200, sim$nb, col = "red")
+legend("topright", legend = c("French Queue", "British Queue"), col = c("black", "red"), lty = 1)
+
+# Plot 2: Expected queuing time over time
+plot(1:7200, sim$eq, type = "l", xlab = "Time (seconds)", ylab = "Expected Queuing Time")
+
+# Plot 3: French and British queue lengths over time
+plot(1:7200, sim_40$nf, type = "l", xlab = "Time (seconds)", ylab = "French Queue Length")
+lines(1:7200, sim_40$nb, col = "red")
+legend("topright", legend = c("French Queue", "British Queue"), col = c("black", "red"), lty = 1)
+
+# Plot 4: Expected queuing time over time
+plot(1:7200, sim_40$eq, type = "l", xlab = "Time (seconds)", ylab = "Expected Queuing Time")
+
+
+# Estimation
+# Calculate the probability that at least one car miss the ferry departure
+missing <- 0
+for (i in 1:100) {
+  qsim(mf=5,mb=5,a.rate=.1,trb=40,trf=40,tmb=40,tmf=30,maxb=20)
+  if (sum(busy) > 0) {
+    missing <- missing + 1
+  }
+}
+
+
+# Findings
+# Comment on the impact of a longer processing time
+
+
+
