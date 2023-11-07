9db2d1f15cc890983233ef417eca180ca1cbf403
Daiki Maruyama <D.Maruyama@sms.ed.ac.uk>
2023-11-07 14:28:16 +0000

    add comment

--- G25.R
+++ G25.R
@@ -1,1 +1,9 @@
-# Frans
+## Frans : s2591760
+## Daiki : s2547603
+## Nathan : s2524152
+
+
+## Contribution to this project
+## Frans (40%): Handled the code for the French and British borders.
+## Daiki (30%): Handled the code for cars going between French and British stations and probabilities.
+## Nathan (30%): Handled the British processing side and plots. 
