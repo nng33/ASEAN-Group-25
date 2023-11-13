062adc8a6f4ac85f91c2d0c4482610942d6f35c7
Daiki Maruyama <D.Maruyama@sms.ed.ac.uk>
2023-11-13 23:14:57 +0000

    write forward

--- G25.R
+++ G25.R
@@ -36,3 +36,49 @@ netup <- function(d) {
   return(nn)
 }
 
+
+#
+forward <- function(nn, inp){
+  nnh <- nn$h
+  nnW <- nn$W
+  nnb <- nn$b
+  
+  # put the input values in the node of the first layer
+  for (i in 1:length(nnh[[1]])){
+    nnh[[1]][i] <- inp[i]
+  }
+  
+  # compute the remaining node values using the ReLU transform
+  for (j in 1:length(nnW)){
+    for (k in 1:nrow(nnW[[j]])){
+      for (m in 1:ncol(nnW[[j]])){
+        if ((nnW[[j]][k, m] * nnh[[j]][k] + nnb[[j]][k]) > 0){
+          nnh[[j+1]][m] <- nnW[[j]][k, m] * nnh[[j]][k] + nnb[[j]][k]
+        }
+        else{
+          nnh[[j+1]][m] <- 0
+        }
+      }
+    }
+  }
+  
+  nn <- list(h = nnh, W = nnW, b = nnb)
+  return(nn)
+}
+
+inp <- c(1,2,3,4)
+
+#
+backward <- function(nn, k){
+  
+  
+}
+
+
+
+#
+train <- function(nn, inp, k, eta=.01, mb=10, nstep=10000){
+  
+  
+  
+}
