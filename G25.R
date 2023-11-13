941aa504f1f0653af69d8876e8dabaff1de70c9c
nng33 <84653912+nng33@users.noreply.github.com>
2023-11-13 15:53:36 +0000

    small changes

--- G25.R
+++ G25.R
@@ -24,12 +24,13 @@ netup <- function(d){
   b <- list()
   
   for (i in 1:length(d)){
-    vec <- rep(c(1), times = length(d[i]))
+    vec <- rep(c(0), times = d[i])
     h[[i]] <- vec
   }
   
   for (j in 1:length(d)-1){
-    W[[j]] <- matrix(1, length(d[j]), length(d[j+1])) + runif(min = 0, max = 0.2)
+    W[[j]] <- matrix(runif(d[j] * d[j+1], min = 0, max = 0.2), 
+                     d[j], d[j+1])
   }
   
   for (k in 1:length(d)){
@@ -39,3 +40,7 @@ netup <- function(d){
   
   return(list(h = h, W = W, b = b))
 }
+
+# Test case for netup
+d <- c(4,8,7,3)
+netup(d)
