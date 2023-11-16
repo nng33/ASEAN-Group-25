4bd707d73b547a569f63681fffd32abea7657940
adlensius <adlensius.djunaedi@outlook.com>
2023-11-16 12:37:03 +0000

    a

---
+++ check.R
@@ -0,0 +1,255 @@
+
+ReLU <- function(h_j) {
+  # Applied the ReLU transform to each element in the list 
+  return(pmax(0, h_j))
+}
+
+forward <- function(nn, inp){
+  # put the values for the first layer in each node
+  nn$h[[1]] <- as.numeric(inp)
+  
+  # compute the remaining node values
+  for(i in 2:length(nn$h)) {
+    nn$h[[i]] <- ReLU(nn$W[[i-1]] %*% nn$h[[i-1]] + nn$b[[i-1]])
+  }
+  
+  return(nn)
+}
+
+backward <- function(nn, k){
+  # loss wrt weights
+  dW <- nn$W
+  
+  # loss wrt node values at each layer
+  dh <- nn$h
+  
+  # loss wrt biases
+  db <- nn$b
+  
+  # Compute the derivative of the loss for k_i w.r.t. h^L_j
+  dh[[length(dh)]] <- softmax(nn$h[[length(nn$h)]])
+  dh[[length(dh)]][k] <- dh[[length(dh)]][k] - 1
+  
+  # Iterate through the number of operations linking the layers together
+  # E.g., if we have a 4-8-7-3, then we have 3 links or number of layers - 1
+  # Backpropagate through the layers to obtain the derivatives
+  
+  for(i in (length(nn$h)-1):1){
+    # mask
+    relu_der <- as.numeric(nn$h[[i+1]] > 0)
+    
+    # Update dh for the current layer
+    dl1 <- dh[[i+1]] * relu_der
+    dh[[i]] <- t(nn$W[[i]]) %*% dl1
+    
+    # Update gradients for weights and biases 
+    dW[[i]] <- dl1 %*% t(nn$h[[i]])
+    db[[i]] <- dl1
+  }
+  
+  nn <- list(h = nn$h, W = nn$W, b = nn$b, dh = dh, dW = dW, db = db)
+  return(nn)
+}
+
+softmax <- function(h_final) {
+  return(exp(h_final)/sum(exp(h_final)))
+}
+
+
+
+train <- function(nn, inp, k, eta=.01, mb=10, nstep=10000){
+  # i have weight
+  # fill in networks with weight 
+  # >> have multiple networks with same weight and biases but different node values
+  # number of networks == number of elements in the minibatch = mb
+  # boo hoo these network sucks
+  # do gradient descent for each network and for each output class k to adjust parameter!
+  # get gradients of everything for each network
+  # get average gradients
+  # STEP
+  # new weight and biases for class k
+  # NEW WEIGHT AND BIASES FOR ALL OUTPUT CLASS :))
+  # fill in ALL network with new weight and biases
+  
+  for (i in 1:nstep){
+    # make the mini batch for this step
+    random_rows <- sample(nrow(inp), size = mb)
+    mini_batch <- inp[random_rows,]
+    k_mb <- k[random_rows]
+    
+    all_nn <- rep(list(nn), mb)
+    
+    # for each element in the mini batch
+    for (j in 1:mb){
+      # fill in nodes according to weights and biases
+      all_nn[[j]] <- forward(all_nn[[j]], inp = mini_batch[j,])
+      
+      # optimize weight and biases with stochastic gradient descent
+      all_nn[[j]] <- backward(all_nn[[j]], k_mb[j])
+    }
+    
+    #   # for each output class
+    #   for (k in 1:length(unique(k))){
+    #     # get gradients of all weights and biases
+    #     all_nn[[j]] <- backward(all_nn[[j]], k)
+    #   }
+    
+    
+    # put all the gradients w.r.t. weight and bias into one list
+    dw_all <- lapply(all_nn, function(x) x$dW)
+    db_all <- lapply(all_nn, function(x) x$db)
+    
+    # find the average gradients
+    sum_dw <- Reduce(function(x, y) Map('+', x, y), dw_all)
+    sum_db <- Reduce(function(x, y) Map('+', x, y), db_all)
+    step_dw <- lapply(sum_dw, function(x) x/mb * eta) # step for W
+    step_db <- lapply(sum_db, function(x) x/mb * eta) # step for b
+    
+    # update weight and bias
+    nn$W <- Map('-', nn$W, step_dw)
+    nn$b <- Map('-', nn$b, step_db)
+    
+  }
+  
+  return(nn)
+}
+
+W <- list(matrix(c(-2.5, -1.5, 0.6, 0.4), nrow = 2),
+          matrix(c(-0.1, 2.4, -2.2, 1.5, -5.2, 3.7), ncol = 2))
+
+b <- list(matrix(c(1.6, 0.7), ncol = 1),
+          matrix(c(0, 0, 1), ncol = 1))
+
+h <- list(matrix(c(0, 0), ncol = 1),
+          matrix(rep(0,2), ncol = 2),
+          matrix(rep(0,3), ncol = 1))
+
+nn <- list(W = W, b = b, h = h)
+nn <- forward(nn1, inp = c(0.5, 0.37))
+n <- backward(nn, k = 1)
+
+dW <- nn$W
+dh <- nn$h
+db <- nn$b
+
+# Compute the derivative of the loss for k_i w.r.t. h^L_j
+
+# fill in the last dh
+# k <- 1
+# dh[[length(dh)]] = softmax(nn$h[[length(nn$h)]])
+# dh[[length(dh)]][k] = dh[[length(dh)]][k] - 1
+# 
+# 
+# for(i in (length(nn$h)-1):1) {
+#   # Compute the derivative of the ReLU function
+#   relu_der <- as.numeric(nn$h[[i+1]] > 0)
+#   # Update dh for the current layer
+#   dl1 <- dh[[i+1]] * relu_der
+#   dh[[i]] <- t(nn$W[[i]]) %*% dl1
+# 
+# 
+#   # Update gradients for weights and biases
+#   dW[[i]] <- dl1 %*% t(nn$h[[i]])
+#   db[[i]] <- dl1
+#   # # Compute the loss for the next iteration
+#   # d_loss <- t(nn$W[[i]]) %*% dh
+# }
+# 
+# # fill in the rest
+# for(i in (length(nn$h)-1):1) {
+#   # Compute the derivative of the ReLU function
+#     relu_der <- as.numeric(nn$h[[i+1]] > 0)
+#     # Update dh for the current layer
+#     dl1 <- dh[[i+1]] * relu_der
+#     dh[[i]] <- t(nn$W[[i]]) %*% dl1
+# }
+# 
+# i <- 2
+# relu_der <- as.numeric(nn$h[[i+1]] > 0)
+# # Update dh for the current layer
+# dl1 <- dh[[i+1]] * relu_der
+# dh[[i]] <- t(nn$W[[i]]) %*% dl1
+# db[[i]] <- dl1
+# dW[[i]] <- dl1 %*% t(nn$h[[i]])
+# 
+# 
+# 
+# i <- 1
+# relu_der <- as.numeric(nn$h[[i+1]] > 0)
+# # Update dh for the current layer
+# dl1 <- dh[[i+1]] * relu_der
+# dh[[i]] <- t(nn$W[[i]]) %*% dl1
+# db[[i]] <- dl1
+# dW[[i]] <- dl1 %*% t(nn$h[[i]])
+
+# train <- function(nn, inp, k, eta=.01, mb=10, nstep=50)
+
+
+# get data
+set.seed(10)
+eta <- 0.01
+data <- iris[sample(nrow(iris),size = 10),c(2,4,5)]
+k <- as.numeric(data[,ncol(data)])
+inp <- data[,-ncol(data)]
+mb <- 3
+
+
+random_rows <- sample(nrow(inp), size = mb)
+mini_batch <- inp[random_rows,]
+k_mb <- k[random_rows]
+
+all_nn <- rep(list(nn), mb)
+
+j <- 1
+all_nn[[j]] <- forward(all_nn[[j]], inp = mini_batch[j,])
+all_nn[[j]] <- backward(all_nn[[j]], k_mb[j])
+
+
+for (i in 1:nstep){
+  # make the mini batch for this step
+  random_rows <- sample(nrow(inp), size = mb)
+  mini_batch <- inp[random_rows,]
+  k_mb <- k[random_rows]
+  
+  all_nn <- rep(list(nn), mb)
+  
+  
+  # for each element in the mini batch
+  for (j in 1:mb){
+    # fill in nodes according to weights and biases
+    all_nn[[j]] <- forward(all_nn[[j]], inp = mini_batch[j,])
+    
+    # optimize weight and biases with stochastic gradient descent
+    all_nn[[j]] <- backward(all_nn[[j]], k_mb[j])
+  }
+  
+  #   # for each output class
+  #   for (k in 1:length(unique(k))){
+  #     # get gradients of all weights and biases
+  #     all_nn[[j]] <- backward(all_nn[[j]], k)
+  #   }
+  
+  
+  # put all the gradients w.r.t. weight and bias into one list
+  dw_all <- lapply(all_nn, function(x) x$dW)
+  db_all <- lapply(all_nn, function(x) x$db)
+  
+  # find the average gradients
+  sum_dw <- Reduce(function(x, y) Map('+', x, y), dw_all)
+  sum_db <- Reduce(function(x, y) Map('+', x, y), db_all)
+  step_dw <- lapply(sum_dw, function(x) x/mb * eta) # step for W
+  step_db <- lapply(sum_db, function(x) x/mb * eta) # step for b
+  
+  # update weight and bias
+  nn$W <- Map('-', nn$W, step_dw)
+  nn$b <- Map('-', nn$b, step_db)
+}
+  
+
+
+
+
+
+
+
+
